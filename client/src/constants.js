d0d801a77ac51deb6b4be18a09754fb6619863f8
Scott So <scottso@uber.com>
2020-12-20 13:23:42 -0500

    home page style

---
+++ client/src/constants.js
@@ -0,0 +1,7 @@
+export const BEIGE = '#E8D2B9';
+export const ORANGE = '#CC8B3C';
+export const MUD_BROWN = '#541F12';
+export const BROWNISH = '#A35E60';
+export const MAIZE = '#F8DF4F';
+
+export const BLUE_CARD = '#38369A';
