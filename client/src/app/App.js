722b82472c01b5e92de0144fc67ee5e1b3083178
Scott So <scottso@uber.com>
2020-12-27 15:32:38 -0500

    add ga

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -3,6 +3,10 @@ import Game from '../component/Game/index';
 import Home from '../component/Home';
 import Room from '../component/Room/index';
 import {BrowserRouter, Route} from 'react-router-dom';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-ZH84JK7WV8');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
 
