20f87183d1ef1e774dae235eb28ee05fbf2468c3
Scott So <scottso@uber.com>
2021-01-13 23:49:06 -0500

    add ts

---
+++ client/src/types.tsx
@@ -0,0 +1,34 @@
+
+export type HintsType = {
+  hint: string,
+  hintCount: number,
+}[];
+
+
+
+export type UserType = {
+  userID: string,
+  team: string,
+  role: string,
+  isHost: boolean,
+  socketId: string,
+};
+
+export type DataType = {
+  colors: string[],
+  words: string[],
+  clicked: Boolean[],
+  isRedTurn: boolean,
+  redTurn: boolean,
+  gameOver: boolean,
+  hints: HintsType,
+  totalGameScore: [number, number],
+  redScore: number,
+  roomID: string,
+  blueScore: number,
+  redSpy: string,
+  blueSpy: string,
+  users: UserType[],
+  userID: string,
+};
+
