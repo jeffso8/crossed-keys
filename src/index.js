513fb82cf44eb2adf10d4aad15371246ffc7178d
Scott So <scottso@uber.com>
2021-05-16 13:43:52 -0400

    test

--- src/index.js
+++ src/index.js
@@ -33,6 +33,7 @@ app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 
 if(process.env.NODE_ENV === 'production') {
   app.use((req, res, next) => {
