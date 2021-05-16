7863381dfe421fedd8c0d3aad9082fea830936f0
Scott So <scottso@uber.com>
2021-05-16 13:24:41 -0400

    try

--- src/index.js
+++ src/index.js
@@ -34,6 +34,15 @@ app.all('*', function(req, res, next) {
   next();
 });
 
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next();
+  })
+}
+
 // enable ssl redirect
 app.use(sslRedirect());
 
